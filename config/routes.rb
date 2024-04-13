9cdc6ea1d84a18a9912e7f4f87123dbeddf9ecff
EC2 Default User <ec2-user@ip-172-31-26-162.ap-northeast-1.compute.internal>
2024-04-13 15:10:59 +0000

    トップ、紹介、新規登録、ログイン画面のデザインの微調整

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,6 @@ Rails.application.routes.draw do
   resources :users, only: [:index, :show, :edit, :update]
   resources :posts do
     resources :comments, only: [:create, :destroy]
-    resource :favorites, only: [:create, :destroy]
+    resource :favoritesgit, only: [:create, :destroy]
   end
 end
